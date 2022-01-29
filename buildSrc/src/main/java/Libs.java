890d06fd6cd5c0db2600302ce5f0796b2ea7f0f2
Admin <itachi_172555@mail.ru>
2022-01-29 21:59:16 +0300

    init foundation

---
+++ buildSrc/src/main/java/Libs.java
@@ -0,0 +1,50 @@
+public class Libs {
+    static String kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_ver";
+    static String corektx = "androidx.core:core-ktx:" + Versions.core_version;
+    static String appcompat = "androidx.appcompat:appcompat:1.3.1";
+    static String material = "com.google.android.material:material:1.4.0";
+    static String constraint = "androidx.constraintlayout:constraintlayout:2.1.1";
+    //navigation
+    static String navFragment = "androidx.navigation:navigation-fragment-ktx:" + Versions.navigation_version;
+    static String navUi = "androidx.navigation:navigation-ui-ktx:" + Versions.navigation_version;
+    //coroutines
+    static String coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:" + Versions.coroutines_version;
+    //ktx
+    static String lifecycleRuntimektx = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0";
+    static String lifecycleViewModelktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0";
+    static String fragmentktx = "androidx.fragment:fragment-ktx:1.3.6";
+    static String activityktx = "androidx.activity:activity-ktx:1.4.0";
+    //test
+    static String junit = "junit:junit:4.13.2";
+    //reflect
+    static String reflect = "org.jetbrains.kotlin:kotlin-reflect:1.5.21";
+    //github.com/square/logcat
+    static String logcat = "com.squareup.logcat:logcat:0.1";
+    //splash
+    static String splashScreen = "androidx.core:core-splashscreen:1.0.0-alpha02";
+    //dagger
+    static String dagger = "com.google.dagger:dagger:" + Versions.dagger_version;
+    static String daggerCompiler = "com.google.dagger:dagger-compiler:" + Versions.dagger_version;
+    //hilt
+    static String hilt = "com.google.dagger:hilt-android:" + Versions.dagger_version;
+    static String hiltCompiler = "com.google.dagger:hilt-compiler:" + Versions.dagger_version;
+    //room
+    static String room = "androidx.room:room-runtime:" + Versions.room_version;
+    static String roomKtx = "androidx.room:room-ktx:" + Versions.room_version;
+    static String roomCompiler = "androidx.room:room-compiler:" + Versions.room_version;
+    //kotlin serialization
+    static String kotlinSerial = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0";
+    static String kotlinSerialConverter = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0";
+    //network
+    static String okHttp = "com.squareup.okhttp3:okhttp:" + Versions.okhttp_version;
+    static String retrofit = "com.squareup.retrofit2:retrofit:" + Versions.retrofit_version;
+    static String logInterceptor = "com.squareup.okhttp3:logging-interceptor:" + Versions.okhttp_version;
+    //coil
+    static String coil = "io.coil-kt:coil:1.4.0";
+    //WorkManager
+    static String workManager = "androidx.work:work-runtime-ktx:2.7.0";
+    static String hiltWork = "androidx.hilt:hilt-work:1.0.0";
+    static String androidxHiltCompiler = "androidx.hilt:hilt-compiler:1.0.0";
+    //Paging
+    static String paging = "androidx.paging:paging-runtime-ktx:3.0.1";
+}
---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,9 @@
+public class Versions {
+    static String core_version = "1.7.0";
+    static String navigation_version = "2.4.0-beta02";
+    static String room_version = "2.3.0";
+    static String retrofit_version = "2.9.0";
+    static String okhttp_version = "5.0.0-alpha.2";
+    static String dagger_version = "2.38.1";
+    static String coroutines_version = "1.5.2-native-mt";
+}
